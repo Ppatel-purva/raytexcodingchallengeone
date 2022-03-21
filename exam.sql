c4e8d3781706c428f8280b910b36eed4bd069c62
Ppatel-purva <70146122+Ppatel-purva@users.noreply.github.com>
2022-03-21 12:18:39 +0530

    Add files via upload

--- exam.sql
+++ exam.sql
@@ -9,3 +9,31 @@ password varchar(50),
 );
 
 SELECT * FROM Userinfo
+
+CREATE TABLE Author(
+id int,
+name varchar(50) NOT NULL,
+password varchar(50),
+);
+
+SELECT * FROM Author
+
+
+CREATE TABLE Bookdetail(
+id int,
+bookname VARCHAR(50) NOT NULL,
+authorname VARCHAR(50),
+bookrecive DATE,
+bookreturn DATE,
+);
+
+SELECT * FROM Bookdetail
+
+
+CREATE TABLE NewBook(
+id int,
+bookname VARCHAR(50) NOT NULL,
+authorname VARCHAR(50),
+);
+
+SELECT * FROM NewBook
